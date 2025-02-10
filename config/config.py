dfbbc90fa99186583e8e01e6fe32af81fdee06d0
Jorge Robledo <robledo.11@hotmail.com>
2025-02-10 14:29:25 -0500

    parametrización

--- config/config.py
+++ config/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent if '__file__' in globals() else Path.cwd()
     DATA_DIR: Path = BASE_DIR / 'data'
+    CONFIG_DIR: Path = BASE_DIR / 'config'
     MODELS_DIR: Path = BASE_DIR / 'models'
     NOTEBOOKS_DIR: Path = BASE_DIR / 'notebooks'
     SRC_DIR: Path = BASE_DIR / 'src'
