c5bc3272b0f5df1e1f8be6f307b38f461ef20354
Jorge Robledo <robledo.11@hotmail.com>
2025-02-12 01:44:58 -0500

    Add changes

--- src/dataset/fetch_data.py
+++ src/dataset/fetch_data.py
@@ -1,2 +1,29 @@
-import json
+import sys
+from pathlib import Path
+sys.path.append(str(Path.cwd().parent.parent))
 from config.config import settings
+import json
+
+
+
+def build_dataset(data_path: str | Path) -> list[dict[str, object]]:
+    """
+    Carga un archivo JSON Lines y devuelve su contenido como una lista de diccionarios.
+
+    Parámetros:
+    -----------
+    data_path : str | Path
+        Ruta al archivo JSON Lines, puede ser una cadena o un objeto Path.
+
+    Retorna:
+    --------
+    list[dict[str, object]]
+        Lista de diccionarios, donde cada diccionario representa un registro del archivo.
+    """
+    # Asegurarse de trabajar con un objeto Path
+    path = Path(data_path) if isinstance(data_path, str) else data_path
+
+    with path.open('r', encoding='utf-8') as f:
+        data = [json.loads(line) for line in f]
+
+    return data
